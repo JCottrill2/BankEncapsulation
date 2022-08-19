dded00250511f6fd822b04800687ff907fbac1a9
jess <jcottrill1123@gmail.com>
2022-08-19 17:01:57 -0400

    initial commit

---
+++ BankEncasulation/Program.cs
@@ -0,0 +1,10 @@
+﻿using BankEncapsulation;
+
+BankAccount myAccount = new BankAccount();
+
+Console.WriteLine("How much would you like to deposit?");
+var amountToDeposit = double.Parse(Console.ReadLine());
+
+myAccount.Deposit(amountToDeposit);
+
+Console.WriteLine(myAccount.GetBalance());
